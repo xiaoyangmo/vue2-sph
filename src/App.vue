14cf5f79d347e3908d85d863cd287a46f45662df
xiaoyangmo <xymxxx@foxmail.com>
2023-03-23 16:35:10 +0800

    create

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header';
+import Footer from './components/Footer';
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+</style>
